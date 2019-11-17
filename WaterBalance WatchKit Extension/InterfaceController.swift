d838aacc53d76650cac8b448c410630c1c87c379
SEisele <simon_eisele@web.de>
2019-11-17 20:37:26 +0100

    Initial Commit

---
+++ WaterBalance WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WaterBalance WatchKit Extension
+//
+//  Created by Simon Eisele on 17.11.19.
+//  Copyright © 2019 Simon Eisele. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
